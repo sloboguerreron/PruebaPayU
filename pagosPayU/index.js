6741e8d0e84edff576440976a70adfeef470d18b
Sebastian <novas.lobog@konradlorenz.edu.co>
2021-08-03 00:30:51 -0500

    payment, charges y autorizacion
    tokenizacion

--- pagosPayU/index.js
+++ pagosPayU/index.js
@@ -1,58 +1,13 @@
-var request = new XMLHttpRequest();
-request.open('POST', 'https://api.paymentsos.com/payments');
-request.setRequestHeader('Content-Type', 'application/json');
-request.setRequestHeader('api-version', '1.3.0');
-request.setRequestHeader('x-payments-os-env', 'test');
-request.setRequestHeader('app-id', 'prueba.wolfbusinesscompany.wolfbusinesscompany');
-request.setRequestHeader('private-key', '63b34924-559d-4a51-b56f-ffe3c2a52874');
-request.setRequestHeader('idempotency-key', 'cust-34532-trans-001356-p');
-var body = {
-  'amount': 34800,
-  'currency': 'EUR',
-  'statement_soft_descriptor': 'Oil lamp'};
-request.send(JSON.stringify(body));
-
-
-var request = new XMLHttpRequest();
-request.open('POST', 'https://api.paymentsos.com/payments/{payment_id}/charges');
-request.setRequestHeader('Content-Type', 'application/json');
-request.setRequestHeader('api-version', '1.3.0');
-request.setRequestHeader('x-payments-os-env', 'test');
-request.setRequestHeader('app-id', 'prueba.wolfbusinesscompany.wolfbusinesscompany');
-request.setRequestHeader('private-key', '63b34924-559d-4a51-b56f-ffe3c2a52874');
-request.setRequestHeader('idempotency-key', 'cust-34532-trans-001356-c');
-var body = {
-  'payment_method': {
-    'type': 'tokenized',
-    'token': '0919e5fd-ed93-4fde-817e-8be7691d5560'}};
-request.send(JSON.stringify(body));
-
-var request = new XMLHttpRequest();
-request.open('POST', 'https://api.paymentsos.com/payments/{payment_id}/authorizations');
-request.setRequestHeader('Content-Type', 'application/json');
-request.setRequestHeader('api-version', '1.3.0');
-request.setRequestHeader('x-payments-os-env', 'test');
-request.setRequestHeader('app-id', 'prueba.wolfbusinesscompany.wolfbusinesscompany');
-request.setRequestHeader('private-key', '63b34924-559d-4a51-b56f-ffe3c2a52874');
-request.setRequestHeader('idempotency-key', 'cust-34532-trans-001356-a');
-var body = {
-  'payment_method': {
-    'type': 'tokenized',
-    'token': '9640e09b-85d0-4509-a19c-90aa65eb386a',
-    'credit_card_cvv': '1231'},
-  'reconciliation_id': '23434534534'};
-request.send(JSON.stringify(body));
-
 const fonts = [
   {
     src: 'https://fonts.googleapis.com/css?family=Source+Code+Pro',
   }
- ]
- const formElements = new POS.Fields("c4d96738-5325-488b-bbc1-54272f0c10cd", {
+]
+const formElements = new POS.Fields("c4d96738-5325-488b-bbc1-54272f0c10cd", {
   fonts
- })
+})
 
- const placeholders = {
+const placeholders = {
   cardNumber: '1234 1234 1234 1234',
   expDate: 'MM / YY'
 }
@@ -69,14 +24,14 @@ const expiry = formElements.create('creditCardExpiry', {
 expiry.mount('#exp-date')
 
 
-document.getElementById('payment-form').addEventListener('submit', async(event) => {
+document.getElementById('payment-form').addEventListener('submit', async (event) => {
   event.preventDefault()
   const additionalData = {
-      holder_name: document.getElementById('cardholder-name').value // This field is mandatory
+    holder_name: document.getElementById('cardholder-name').value // This field is mandatory
   }
   const result = await POS.createToken(cardNumber, {
-      additionalData,
-      environment: 'test' // Set the PaymentsOS environment you're connecting to
+    additionalData,
+    environment: 'test' // Set the PaymentsOS environment you're connecting to
   })
   console.log(`The response is ${JSON.stringify(result)}`)
 })
@@ -97,11 +52,88 @@ const classes = {
   invalid: 'my-own-invalid-class'
 }
 
-const cardNumber = formElements.create('cardNumber', {
+/*const cardNumber = formElements.create('cardNumber', {
   classes,
-})
+})*/
 
-const cardNumber = formElements.create('cardNumber', {
+/*const cardNumber = formElements.create('cardNumber', {
   style,
-})
+})*/
 
+function prueba() {
+  POS.tokenize({
+    "token_type": "credit_card",
+    "holder_name": "MR C D HOLDER",
+    "expiration_date": "09-2025",
+    "card_number": "4111111111111111"
+    /* "token_type": "credit_card",
+     "holder_name": document.getElementById("cardholder-name"),
+     "expiration_date": document.getElementById("exp-date"),
+     "card_number": document.getElementById("card-number")*/
+  },
+    function (result) {
+
+      console.log("result obtained" + result);
+      console.log('sirve');
+      let json = JSON.parse(result);
+      token = json["token"];
+      console.log(json["token"]);
+
+      // Check for errors, if all is good, then proceed!
+    }
+  );
+
+
+  //payment
+  var request = new XMLHttpRequest();
+  request.open('POST', 'https://api.paymentsos.com/payments');
+  request.withCredentials = true;
+  request.setRequestHeader('api-version', '1.3.0');
+  request.setRequestHeader('x-payments-os-env', 'test');
+  request.setRequestHeader('app-id', 'prueba.wolfbusinesscompany.wolfbusinesscompany');
+  request.setRequestHeader('private-key', '63b34924-559d-4a51-b56f-ffe3c2a52874');
+  request.setRequestHeader('idempotency-key', 'cust-34532-trans-001356-p');
+
+  var body = {
+    'amount': 34800,
+    'currency': 'USD',
+    'statement_soft_descriptor': 'Oil lamp'
+  };
+  request.send(JSON.stringify(body));
+
+  //cargar
+  var request = new XMLHttpRequest();
+  request.open('POST', 'https://api.paymentsos.com/payments/{payment_id}/charges');
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.setRequestHeader('api-version', '1.3.0');
+  request.setRequestHeader('x-payments-os-env', 'test');
+  request.setRequestHeader('app-id', 'prueba.wolfbusinesscompany.wolfbusinesscompany');
+  request.setRequestHeader('private-key', '63b34924-559d-4a51-b56f-ffe3c2a52874');
+  request.setRequestHeader('idempotency-key', 'cust-34532-trans-001356-c');
+  var body = {
+    'payment_method': {
+      'type': 'tokenized',
+      'token': token
+    }
+  };
+  request.send(JSON.stringify(body));
+
+  //autorizacion
+  var request = new XMLHttpRequest();
+  request.open('POST', 'https://api.paymentsos.com/payments/{payment_id}/authorizations');
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.setRequestHeader('api-version', '1.3.0');
+  request.setRequestHeader('x-payments-os-env', 'test');
+  request.setRequestHeader('app-id', 'prueba.wolfbusinesscompany.wolfbusinesscompany');
+  request.setRequestHeader('private-key', '63b34924-559d-4a51-b56f-ffe3c2a52874');
+  request.setRequestHeader('idempotency-key', 'cust-34532-trans-001356-a');
+  var body = {
+    'payment_method': {
+      'type': 'tokenized',
+      'token':  token,
+      'credit_card_cvv': '1231'
+    },
+    'reconciliation_id': '23434534534'
+  };
+  request.send(JSON.stringify(body));
+}
